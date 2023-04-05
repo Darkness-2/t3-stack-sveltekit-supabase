7244292b11e107b130af489d09cc70d13bb27111
Darkness-2 <justinmmm690@gmail.com>
2023-04-05 10:44:30 -0400

    Move userStore to use context API

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,8 @@
 	import '../app.css';
 	import { browser } from '$app/environment';
 	import { QueryClient, QueryClientProvider } from '@tanstack/svelte-query';
+	import { createUserStore } from '$lib/stores/userStore';
+	import { setContext } from 'svelte';
 
 	const queryClient = new QueryClient({
 		defaultOptions: {
@@ -10,6 +12,14 @@
 			}
 		}
 	});
+
+	/**
+	 * Create the userStore and attach it to a context.
+	 * Ensures stores are unique per request.
+	 * See https://kit.svelte.dev/docs/state-management#using-stores-with-context
+	 */
+	const userStore = createUserStore();
+	setContext('userStore', userStore);
 </script>
 
 <QueryClientProvider client={queryClient}>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,8 @@
+<script lang="ts">
+	import { getUserStore } from '$lib/stores/userStore';
+
+	const userStore = getUserStore();
+</script>
+
 <h1>Welcome to T3 SvelteKit!</h1>
+{$userStore.user?.id}
