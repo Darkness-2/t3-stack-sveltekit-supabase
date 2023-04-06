69efb7f0c1802546b614d36e639629ab88594728
Darkness-2 <justinmmm690@gmail.com>
2023-04-06 19:43:44 -0400

    Move tRPC handler from hooks to api route

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,23 @@
 <script lang="ts">
+	import { trpc } from '$lib/client/trpc';
 	import { getUserStore } from '$lib/stores/userStore';
+	import { createQuery } from '@tanstack/svelte-query';
 
 	const userStore = getUserStore();
+
+	const helloQuery = createQuery({
+		queryKey: ['hello'],
+		queryFn: () => trpc.hello.query()
+	});
 </script>
 
 <h1>Welcome to T3 SvelteKit!</h1>
-{$userStore.user?.id}
+<p>User ID: {$userStore.user?.id}</p>
+
+{#if $helloQuery.isLoading}
+	<p>Loading tRPC...</p>
+{:else if !$helloQuery.data}
+	<p>Something went wrong...</p>
+{:else}
+	<p>Greeting: {$helloQuery.data}</p>
+{/if}
